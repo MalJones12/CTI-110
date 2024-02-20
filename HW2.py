ea69c1e9980cfe7f5cafe5b68bc32bdfd10267e6
MalJones12 <160150709+MalJones12@users.noreply.github.com>
2024-02-20 16:38:36 +0000

    test

--- HW2.py
+++ HW2.py
@@ -3,12 +3,11 @@
 #Program Detail - Input/Output
 
 #Street is a string datatype
-Street = input("What is your street? ")
+street = input("What is your street? ")
 
 #Convert to int() in the same line
 house_num = int(input("What is your house number? "))
 
-house_num = input("What is your house number? ")
 
 #Convert to int() seperately
 house_num = int(house_num)
