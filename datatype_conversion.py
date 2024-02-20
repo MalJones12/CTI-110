ae90e41cb030e8ef997f2dfd49132e76c6cf2c4b
MalJones12 <160150709+MalJones12@users.noreply.github.com>
2024-02-20 15:57:38 +0000

    upload

---
+++ datatype_conversion.py
@@ -0,0 +1,28 @@
+'''
+#convering datatype
+
+weekly_rate = input("Enter your weekly pay: ")
+
+weekly_rate = float(weekly_rate)
+
+print("Your weekly pay is", weekly_rate)
+
+#See what the datatype is for a ariable
+print(type(weekly_rate))
+
+#Display bi-weekly pay
+print("Every two weeks you make", "$" + str(weekly_rate * 2), "dollars")
+
+'''
+
+#In your hw, do not hardvode. Get values from user. Remember to convertto a number datay=type
+
+num1 = 3
+num2 = 5
+
+
+print(num1, "*", num2, "=", num1 * num2)
+
+#Using exponents
+
+print(num1, "raised to the power of", num2, "is", num1**num2)
